fbcc2a6e885f98f8c61caa2c13bf37667b78aae8
AidenVargas03 <stingrayav13@gmail.com>
2026-05-31 19:18:27 -0700

    Fix CORS add Vercel URL

--- WaferApi/Program.cs
+++ WaferApi/Program.cs
@@ -1,11 +1,12 @@
-using WaferApi.Services;
+﻿using WaferApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173",
+            "https://wafer-defect-visualizer.vercel.app")
               .AllowAnyHeader()
               .AllowAnyMethod());
 });
